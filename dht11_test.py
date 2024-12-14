c6aba2fc3e0cbb38887dbf708b0cfeb0f9ee7b2e
Miso Kim <152621906+oosim@users.noreply.github.com>
2024-12-14 21:10:06 +0900

    Update dht11_test.py

--- dht11_test.py
+++ dht11_test.py
@@ -1,20 +1,68 @@
-import Adafruit_DHT  # 라이브러리 불러오기
+import time
 
-sensor = Adafruit_DHT.DHT11  # sensor 객체 생성
+import board
 
-pin = 4  # Data핀의 GPIO핀 넘버
+import adafruit_dht
 
-humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)  # 센서 객체에서 센서 값(ㅇ노도, 습도) 읽기
+​
 
-if humidity is not None and temperature is not None:  # 습도 및 온도 값이 모두 제대로 읽혔다면
-    print(
+# Initial the dht device, with data pin connected to:
 
-        "Temp: {:.1f} C    Humidity: {}% ".format(
+dhtDevice = adafruit_dht.DHT11(board.D4)
 
-            temperature, humidity
+​
+
+# you can pass DHT22 use_pulseio=False if you wouldn't like to use pulseio.
+
+# This may be necessary on a Linux single board computer like the Raspberry Pi,
+
+# but it will not work in CircuitPython.
+
+# dhtDevice = adafruit_dht.DHT22(board.D18, use_pulseio=False)
+
+​
+
+while True:
+
+    try:
+
+        # Print the values to the serial port
+
+        temperature_c = dhtDevice.temperature
+
+        temperature_f = temperature_c * (9 / 5) + 32
+
+        humidity = dhtDevice.humidity
+
+        print(
+
+            "Temp: {:.1f} F / {:.1f} C    Humidity: {}% ".format(
+
+                temperature_f, temperature_c, humidity
+
+            )
 
         )
 
-    )  # 온도, 습도 순으로 표시
-else:
-    print('Failed to get reading. Try again!')
+​
+
+    except RuntimeError as error:
+
+        # Errors happen fairly often, DHT's are hard to read, just keep going
+
+        print(error.args[0])
+
+        time.sleep(2.0)
+
+        continue
+
+    except Exception as error:
+
+        dhtDevice.exit()
+
+        raise error
+
+​
+
+    time.sleep(2.0)
+[출처] 라즈베리파이5와 온습도센서 연결하기|작성자 해피코드
